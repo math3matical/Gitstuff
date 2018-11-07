a4ba3c375ee1d85347375ca55dd95d767eaf3f90
Michael Yoder <myoder@redhat.com>
2018-11-07 14:43:56 -0500

    removed 2 domains

--- IGT/Integration/RetirementDate/DynamicDialogs.class/__methods__/calendar.rb
+++
@@ -1,6 +0,0 @@
-retirement_date_type = nil
-$evm.root.attributes.detect { |k,v| retirement_date_type = v if k.end_with?('retirement_date_type')}
-
-if retirement_date_type == "end_date"
-  $evm.object['visible'] = true
-end
